e2f161d8081a5383b80711172ed4884d16123339
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 18:15:13 -0300

    feat: implentação do module de usuário a aplicação nestjs

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/database.module';
+import { ModuleUser } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [PrismaModule, ModuleUser],
 })
 export class AppModule {}
