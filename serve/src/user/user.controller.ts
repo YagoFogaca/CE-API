b837c34fd58535582044b93aeb12ec50c8ac1b80
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 18:26:37 -0300

    fix: console para mensagem de error

--- serve/src/user/user.controller.ts
+++ serve/src/user/user.controller.ts
@@ -21,7 +21,7 @@ export class ControllerUser {
     try {
       return await this.serviceUser.create(user);
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       HandleExceptions(err);
     }
   }
@@ -31,7 +31,7 @@ export class ControllerUser {
     try {
       return await this.serviceUser.delete(id);
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       HandleExceptions(err);
     }
   }
@@ -41,7 +41,7 @@ export class ControllerUser {
     try {
       return await this.serviceUser.findAll();
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       HandleExceptions(err);
     }
   }
@@ -51,7 +51,7 @@ export class ControllerUser {
     try {
       return await this.serviceUser.findById(id);
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       HandleExceptions(err);
     }
   }
@@ -61,7 +61,7 @@ export class ControllerUser {
     try {
       return await this.serviceUser.update(user, id);
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       HandleExceptions(err);
     }
   }
