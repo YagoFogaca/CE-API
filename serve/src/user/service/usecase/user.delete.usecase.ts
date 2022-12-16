26182d1334dad93dd2e5232d2a9e9bc943e1bff7
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 18:50:51 -0300

    feat: tratativa de error ao deletar um usuário

--- serve/src/user/service/usecase/user.delete.usecase.ts
+++ serve/src/user/service/usecase/user.delete.usecase.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { IUserEntity } from 'src/user/entities/user.entity';
+import { Exception } from 'src/utils/exceptions/exception';
+import { Exceptions } from 'src/utils/exceptions/exceptions.parms';
 
 @Injectable()
 export class DeleteUserUsecase {
   constructor(private readonly prismaService: PrismaService) {}
 
   async execute(id: string): Promise<IUserEntity> {
-    return await this.prismaService.user.delete({ where: { id: id } });
+    try {
+      return await this.prismaService.user.delete({ where: { id: id } });
+    } catch (err) {
+      throw new Exception(
+        Exceptions.NotFoundData,
+        'Nenhum usuário com esse ID',
+      );
+    }
   }
 }
