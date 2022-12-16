96aac301d153d18ffe5005a01c1a8f88f7737242
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 17:48:01 -0300

    feat: criação do module de usuário

---
+++ serve/src/user/user.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common/decorators';
+import { PrismaModule } from 'src/prisma/database.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserUsecase } from './service/usecase/user.create.usecase';
+import { DeleteUserUsecase } from './service/usecase/user.delete.usecase';
+import { FindAllUserUsecase } from './service/usecase/user.findAll.usecase';
+import { FindByIdUserUsecase } from './service/usecase/user.findById.usercase';
+import { UpdateUserUsecase } from './service/usecase/user.update.usecase';
+import { UserRepository } from './service/user.repository';
+import { ServiceUser } from './service/user.service';
+import { ControllerUser } from './user.controller';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [ControllerUser],
+  providers: [
+    ServiceUser,
+    CreateUserUsecase,
+    DeleteUserUsecase,
+    FindAllUserUsecase,
+    FindByIdUserUsecase,
+    UpdateUserUsecase,
+    UserRepository,
+    PrismaService,
+  ],
+  exports: [ServiceUser, UserRepository],
+})
+export class ModuleUser {}
