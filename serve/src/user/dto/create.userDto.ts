75d37c22bc144f5259ba92ee2a17ee1ba78c070b
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 18:32:47 -0300

    feat: implementação do class validator

--- serve/src/user/dto/create.userDto.ts
+++ serve/src/user/dto/create.userDto.ts
@@ -1,6 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   nome: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   nome_usuario: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   senha: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   role: string;
 }
