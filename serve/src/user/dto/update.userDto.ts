85fd3330b11e3dbe191a4d807d735147e536362a
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 18:47:26 -0300

    fix: id obrigatorio para atualizar um usuário

--- serve/src/user/dto/update.userDto.ts
+++ serve/src/user/dto/update.userDto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { CreateUserDto } from './create.userDto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
