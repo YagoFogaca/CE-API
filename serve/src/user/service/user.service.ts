b0590701958ac6c9a3867c9776a805c107071255
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-16 17:07:22 -0300

    feat: criação do service de usuários

---
+++ serve/src/user/service/user.service.ts
@@ -0,0 +1,38 @@
+import { CreateUserDto } from '../dto/create.userDto';
+import { UpdateUserDto } from '../dto/update.userDto';
+import { IUserEntity } from '../entities/user.entity';
+import { CreateUserUsecase } from './usecase/user.create.usecase';
+import { DeleteUserUsecase } from './usecase/user.delete.usecase';
+import { FindAllUserUsecase } from './usecase/user.findAll.usecase';
+import { FindByIdUserUsecase } from './usecase/user.findById.usercase';
+import { UpdateUserUsecase } from './usecase/user.update.usecase';
+
+export class ServiceUser {
+  constructor(
+    private readonly createUserUsecase: CreateUserUsecase,
+    private readonly deleteUserUsecase: DeleteUserUsecase,
+    private readonly findAllUserUsecase: FindAllUserUsecase,
+    private readonly findByIdUserUsecase: FindByIdUserUsecase,
+    private readonly updateUserUsecase: UpdateUserUsecase,
+  ) {}
+
+  async create(user: CreateUserDto): Promise<IUserEntity> {
+    return await this.createUserUsecase.execute(user);
+  }
+
+  async delete(id: string): Promise<string> {
+    await this.deleteUserUsecase.execute(id);
+    return 'Usuário deletado com sucesso';
+  }
+
+  async findAll(): Promise<IUserEntity[]> {
+    return await this.findAllUserUsecase.execute();
+  }
+  async findById(id: string): Promise<IUserEntity> {
+    return await this.findByIdUserUsecase.execute(id);
+  }
+
+  async update(user: UpdateUserDto, id: string): Promise<IUserEntity> {
+    return await this.updateUserUsecase.execute({ ...user, id });
+  }
+}
