e23ca0334e65c952351e94429b9e0ed6bbe2eaf3
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-19 19:51:45 -0300

    fix: remoção de teste de tipagem

--- serve/src/supply/dto/create.supplyDto.ts
+++ serve/src/supply/dto/create.supplyDto.ts
@@ -5,11 +5,3 @@ export class CreateSupplyDto {
   ativo: boolean;
   id_user: string;
 }
-
-const teste: CreateSupplyDto = {
-  nome: '',
-  quant_estoque: 0,
-  unidade: '',
-  ativo: false,
-  id_user: '',
-};
