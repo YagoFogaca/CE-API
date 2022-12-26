50142593e1d92227c0fe5ec975733f4e4d7e1caa
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-26 10:09:46 -0300

    feat: implementação do Supply usecase e importação do modulo de user

--- serve/src/supply/supply.module.ts
+++ serve/src/supply/supply.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/prisma/database.module';
+import { ServiceUser } from 'src/user/service/user.service';
+import { ModuleUser } from 'src/user/user.module';
 import { SupplyRepository } from './service/supply.repository';
 import { SupplyService } from './service/supply.service';
+import { CreateSupplyUsecase } from './service/usecase/supply.create.usecase';
 import { SupplyController } from './supply.controller';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [SupplyController],
-  providers: [SupplyRepository, SupplyService],
+    imports: [DatabaseModule, ModuleUser],
+    controllers: [SupplyController],
+    providers: [SupplyRepository, SupplyService, CreateSupplyUsecase],
 })
 export class SupplyModule {}
