cf98579d3f558874edf39cc389f1079a89c752bc
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-26 09:17:27 -0300

    fix: Prettierrc

--- serve/src/supply/service/supply.repository.ts
+++ serve/src/supply/service/supply.repository.ts
@@ -5,37 +5,37 @@ import { ISupplyEntity } from '../entities/supply.entity';
 
 @Injectable()
 export class SupplyRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+    constructor(private readonly prismaService: PrismaService) {}
 
-  async create(supply: CreateSupplyDto, id: string): Promise<ISupplyEntity> {
-    return await this.prismaService.supply.create({
-      data: {
-        id: id,
-        nome: supply.nome,
-        quant_estoque: supply.quant_estoque,
-        unidade: supply.unidade,
-        ativo: supply.ativo,
-        id_user: supply.id_user,
-      },
-      include: {
-        user: true,
-        entrySupply: true,
-        exitSupply: true,
-      },
-    });
-  }
+    async create(supply: CreateSupplyDto, id: string): Promise<ISupplyEntity> {
+        return await this.prismaService.supply.create({
+            data: {
+                id: id,
+                nome: supply.nome,
+                quant_estoque: supply.quant_estoque,
+                unidade: supply.unidade,
+                ativo: supply.ativo,
+                id_user: supply.id_user,
+            },
+            include: {
+                user: true,
+                entrySupply: true,
+                exitSupply: true,
+            },
+        });
+    }
 
-  async findAll(): Promise<ISupplyEntity[]> {
-    return await this.prismaService.supply.findMany({
-      include: {
-        user: true,
-        entrySupply: true,
-        exitSupply: true,
-      },
-    });
-  }
+    async findAll(): Promise<ISupplyEntity[]> {
+        return await this.prismaService.supply.findMany({
+            include: {
+                user: true,
+                entrySupply: true,
+                exitSupply: true,
+            },
+        });
+    }
 
-  // async findById(id: string): Promise<IUserEntity> {
-  //   return await this.prismaService.user.findFirst({ where: { id: id } });
-  // }
+    // async findById(id: string): Promise<IUserEntity> {
+    //   return await this.prismaService.user.findFirst({ where: { id: id } });
+    // }
 }
