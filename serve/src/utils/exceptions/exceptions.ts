61140602ca032693275358af85ef43fb0e667ab5
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-12 14:38:01 -0300

    feat: update HandleExceptions para suportar NotFoundException

--- serve/src/utils/exceptions/exceptions.ts
+++ serve/src/utils/exceptions/exceptions.ts
@@ -1,22 +1,24 @@
 import {
-  BadRequestException,
-  InternalServerErrorException,
-  UnauthorizedException,
+   BadRequestException,
+   InternalServerErrorException,
+   UnauthorizedException,
+   NotFoundException,
 } from '@nestjs/common';
 import { ParmsExceptions, Exceptions } from './exceptions.parms';
 
 export function HandleExceptions({ exception, message }: ParmsExceptions) {
-  if (
-    exception === Exceptions.InvalidData ||
-    exception === Exceptions.NotFoundData
-  ) {
-    throw new BadRequestException(message);
-  }
+   if (exception === Exceptions.InvalidData) {
+      throw new BadRequestException(message);
+   }
 
-  if (exception === Exceptions.DatabaseException) {
-    throw new InternalServerErrorException(message);
-  }
-  if (exception === Exceptions.UnauthorizedException) {
-    throw new UnauthorizedException(message);
-  }
+   if (exception === Exceptions.NotFoundData) {
+      throw new NotFoundException(message);
+   }
+
+   if (exception === Exceptions.DatabaseException) {
+      throw new InternalServerErrorException(message);
+   }
+   if (exception === Exceptions.UnauthorizedException) {
+      throw new UnauthorizedException(message);
+   }
 }
