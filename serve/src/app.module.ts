38ff7049628d1146dcf7511a444d9bad27a14857
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-12 19:44:19 -0300

    feat: implementação da Feature de entradas de insumos

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EntrySupplyModule } from './entry/entry.module';
 import { DatabaseModule } from './prisma/database.module';
 import { SupplyModule } from './supply/supply.module';
 import { ModuleUser } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, ModuleUser, SupplyModule],
+   imports: [DatabaseModule, ModuleUser, SupplyModule, EntrySupplyModule],
 })
 export class AppModule {}
