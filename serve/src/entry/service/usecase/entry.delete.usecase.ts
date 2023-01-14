d79d06a21657c4c43a5f1ee3cbac76c1770463f6
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-14 10:29:25 -0300

    feat: metodo de deletar entrada diminui estoque

--- serve/src/entry/service/usecase/entry.delete.usecase.ts
+++ serve/src/entry/service/usecase/entry.delete.usecase.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { IEntryEntity } from 'src/entry/entities/entry.entity';
+import { SupplyService } from 'src/supply/service/supply.service';
 import { Exception } from 'src/utils/exceptions/exception';
 import { Exceptions } from 'src/utils/exceptions/exceptions.parms';
 import { EntryRepository } from '../entry.repository';
 
 @Injectable()
 export class DeleteEntrySupplyUsecase {
-   constructor(private readonly entryRepository: EntryRepository) {}
+   constructor(
+      private readonly entryRepository: EntryRepository,
+      private readonly serviceSupply: SupplyService,
+   ) {}
 
    async execute(id: string): Promise<IEntryEntity> {
       try {
-         return await this.entryRepository.delete(id);
+         const entryDeleted = await this.entryRepository.delete(id);
+         const updateQuant =
+            entryDeleted.supply.quant_estoque - entryDeleted.quant;
+         await this.serviceSupply.update(entryDeleted.supply.id, {
+            quant_estoque: updateQuant,
+         });
+
+         return entryDeleted;
       } catch (err) {
          throw new Exception(
             Exceptions.NotFoundData,
