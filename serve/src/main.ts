824adba3e16c3598071908fc2046735d406df17d
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-18 21:14:56 -0300

    fix: formatação

--- serve/src/main.ts
+++ serve/src/main.ts
@@ -5,16 +5,17 @@ import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+   const app = await NestFactory.create(AppModule);
+   app.useGlobalPipes(new ValidationPipe());
+   app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Controle de Estoque')
-    .setVersion('1.0.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+   const config = new DocumentBuilder()
+      .setTitle('Controle de Estoque')
+      .setVersion('1.0.0')
+      .build();
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+   await app.listen(3000);
 }
 bootstrap();
